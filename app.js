42a5fc915efbf0771e92cf3cba455e0df8a2231a
eduardsdn <eduard.sdn@gmail.com>
2023-01-24 14:50:47 +0100

    Drafted updateGameBoard method of GameBoard object constructor

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
+
+
 function Game(){
-
+    
 }
 
-function Player(){
-
+function Player(marker){
+    this.markTile = function(){
+        
+    }
 }
 
+function GameBoard(){
+
+    this.updateGameBoard = function(){
+        tiles = document.querySelectorAll(".tile")
+        console.log(tiles)
+        
+        marks = [0,1,0,1,0,1,0,1,0]
+
+        tilesArray = [].slice.call(tiles, 0)
+
+        for(let i = 0; i < marks.length; i++){
+            if(marks[i] === 0){
+                tilesArray[i].innerText ="O"
+            }
+
+            else if(marks[i] === 1){
+                tilesArray[i].innerText ="X"
+            }
+        }
+        
+    }
+}
+
+gameBoard = new GameBoard
+
+gameBoard.updateGameBoard()
+
+
+
+// tiles.forEach(function(tile){
+//     tilesArray = [0,1,0,1,0,1,0,1,0]
+//     for(let i = 0; i < tilesArray.length; i++){
+//         if(tilesArray[i] === 0){
+//             tile.innerText = "O"
+//         }
+//         else if(tilesArray[i] === 1){
+//             tile.innerText[i] === "X"
+//         }
+//     }
+// }) 
