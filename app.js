245e517d0de9650c15a9b9e9e46a358d3881a547
eduardsdn <eduard.sdn@gmail.com>
2023-01-26 13:32:00 +0100

    Drafted Game object. Tiles are now markable on the game board. Game logic is yet to be developed

--- app.js
+++ app.js
@@ -1,69 +1,113 @@
-
+// for marks 0 = "O" 1 = "X" 2 = Blank tile
 
 function Game(){
 
+    // this.createPlayers = function(){
+    //     player1 = new Player("X")
+    //     player2 = new Player("O")
+    //     return [player1, player2]
+    // }
+
+    playerX = new Player("X")
+    playerO = new Player("O")
+
+    gameBoard = new GameBoard
+
+    playerO.markTile(gameBoard.getMarks())
+    
+    gameBoard.updateGameBoard()
+
 }
 
-// supposively player should haver marker attribute
-function Player(){             
-    this.markTile = function(){
-        // marks here temporary
-        marks = [0,1,0,1,0,1,0,1,0] 
+
+function Player(marker){             
+
+    this.marker = marker
+
+    this.markTile = function(marks){
+    
+        if (this.marker === "X"){
+            marker = 1
+        }
+        else if(this.marker === "O"){
+            marker = 0
+        }
 
         tiles = document.querySelectorAll(".tile")
         tilesArray = [].slice.call(tiles, 0)
         for(let i = 0; i < tilesArray.length; i++){
             tilesArray[i].addEventListener('click', function(){
                 if(tilesArray[i].id === "one"){
-                    marks[i] = 1
-                    console.log(marks)
+                    marks[0] = marker
+                    gameBoard.updateGameBoard()
+                    // console.log(gameBoard.marks)
                 }
+                else if(tilesArray[i].id === "two"){
+                    marks[1] = marker
+                    gameBoard.updateGameBoard()
+                }
+                else if(tilesArray[i].id === "three"){
+                    marks[2] = marker
+                    gameBoard.updateGameBoard()
+                }
+                else if(tilesArray[i].id === "four"){
+                    marks[3] = marker
+                    gameBoard.updateGameBoard()
+                }
+                else if(tilesArray[i].id === "five"){
+                    marks[4] = marker
+                    gameBoard.updateGameBoard()
+                }
+                else if(tilesArray[i].id === "six"){
+                    marks[5] = marker
+                    gameBoard.updateGameBoard()
+                }
+                else if(tilesArray[i].id === "seven"){
+                    marks[6] = marker
+                    gameBoard.updateGameBoard()
+                }
+                else if(tilesArray[i].id === "eight"){
+                    marks[7] = marker
+                    gameBoard.updateGameBoard()
+                }
+                else if(tilesArray[i].id === "nine"){
+                    marks[8] = marker
+                    gameBoard.updateGameBoard()
+                }        
             })
-        }
-        return marks
+        } 
     }
 }
+
 
 function GameBoard(){
 
+    this.marks = [2,2,2,2,2,2,2,2,2]
+
     this.updateGameBoard = function(){
         tiles = document.querySelectorAll(".tile")
-        console.log(tiles)
+        // console.log(tiles)
         
-        marks = [0,1,0,1,0,1,0,1,0]
+        // marks here temporary (need to bring them to either Game obj or some other outer scope or obj)
+        
 
         tilesArray = [].slice.call(tiles, 0)
 
-        for(let i = 0; i < marks.length; i++){
-            if(marks[i] === 0){
+        for(let i = 0; i < this.marks.length; i++){
+            if(this.marks[i] === 0){
                 tilesArray[i].innerText ="O"
             }
 
-            else if(marks[i] === 1){
+            else if(this.marks[i] === 1){
                 tilesArray[i].innerText ="X"
             }
         }
-        
+    }
+
+    this.getMarks = function(){
+        return this.marks
     }
 }
 
-gameBoard = new GameBoard
-player = new Player
+game = new Game
 
-player.markTile()
-
-// gameBoard.updateGameBoard()
-
-
-
-// tiles.forEach(function(tile){
-//     tilesArray = [0,1,0,1,0,1,0,1,0]
-//     for(let i = 0; i < tilesArray.length; i++){
-//         if(tilesArray[i] === 0){
-//             tile.innerText = "O"
-//         }
-//         else if(tilesArray[i] === 1){
-//             tile.innerText[i] === "X"
-//         }
-//     }
-// }) 
