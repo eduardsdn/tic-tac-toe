befcccf30b787e3aabe410d5609599379caa8907
eduardsdn <eduard.sdn@gmail.com>
2023-02-06 15:44:26 +0100

    checkWin() is now a method ofGame object

--- app.js
+++ app.js
@@ -5,6 +5,42 @@ function Game(){
     this.swapTurn = function(){
         game.whosTurn = !this.whosTurn
     }
+
+    this.winningCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
+
+    this.checkWin = function(){
+
+        tilesArray = Array.prototype.slice.call(gameBoard.tiles)
+        for(let i = 0; i < this.winningCombinations.length; i++){
+            let counterX = 0
+            let counterO = 0
+            for(let j = 0; j < this.winningCombinations[i].length; j++){
+                if(tilesArray[this.winningCombinations[i][j]].classList.contains("X")){
+                    counterX ++
+                }
+                if(counterX === 3){
+                    console.log("X-win")
+                }
+    
+                if(tilesArray[this.winningCombinations[i][j]].classList.contains("O")){
+                    counterO ++
+                }
+                if(counterO === 3){
+                    console.log("O-win")
+                }
+            }
+            
+        }    
+    }
 }
 
 function Player(marker){
@@ -23,7 +59,7 @@ function GameBoard(){
         const currentPlayer = game.whosTurn ? playerX.marker : playerO.marker
         gameBoard.MarkTile(tile, currentPlayer)
         game.swapTurn()
-        checkWin()
+        game.checkWin()
     }
 
     this.tiles.forEach(tile => {
@@ -39,42 +75,10 @@ const game = new Game
 
 const gameBoard = new GameBoard
 
-const winningCombinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-]
 
 
-let checkWin = function(){
 
-    tilesArray = Array.prototype.slice.call(gameBoard.tiles)
-    for(let i = 0; i < winningCombinations.length; i++){
-        let counterX = 0
-        let counterO = 0
-        for(let j = 0; j < winningCombinations[i].length; j++){
-            if(tilesArray[winningCombinations[i][j]].classList.contains("X")){
-                counterX ++
-            }
-            if(counterX === 3){
-                console.log("X-win")
-            }
 
-            if(tilesArray[winningCombinations[i][j]].classList.contains("O")){
-                counterO ++
-            }
-            if(counterO === 3){
-                console.log("O-win")
-            }
-        }
-        
-    }    
- }
 
 // && winningCombinations[i][j] === tilesArray[j]
 
